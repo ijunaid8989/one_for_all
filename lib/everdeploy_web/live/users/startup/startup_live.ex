f5931639ec109aeb98e97b998152fbd2df4dabce
Junaid Farooq <junaid@evercam.io>
2020-06-06 21:54:18 +0500

    Added dummy validate && save events.

--- lib/everdeploy_web/live/users/startup/startup_live.ex
+++ lib/everdeploy_web/live/users/startup/startup_live.ex
@@ -5,4 +5,12 @@ defmodule EverdeployWeb.StartupLive do
   def mount(_params, _session, socket) do
     {:ok, assign(socket, query: "")}
   end
+
+  def handle_event("validate", %{"user" => user} = params, socket) do
+    {:noreply, assign(socket, changeset: %{})}
+  end
+
+  def handle_event("save", %{"user" => user} = params, socket) do
+    {:noreply, assign(socket, changeset: %{})}
+  end
 end
