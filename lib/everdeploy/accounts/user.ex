c4680e9eb025732a0a5bc0c6241e9e714f6e1b99
Junaid Farooq <junaid@evercam.io>
2020-06-07 10:39:09 +0500

    Added Accounts User context.

---
+++ lib/everdeploy/accounts/user.ex
@@ -0,0 +1,26 @@
+defmodule Everdeploy.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @email_regex ~r/^[A-Za-z0-9._%+-]+@evercam.io$/
+
+  schema "users" do
+    field :email, :string
+    field :password, :string
+    field :sign_in_count, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password, :sign_in_count])
+    |> unique_constraint(:email, [name: :user_email_unique_index, message: "Email has already been taken."])
+    |> validate_required([:email], message: "Email cannot be blank.")
+    |> validate_required([:password], message: "Password cannot be blank.")
+    |> update_change(:email, &String.downcase/1)
+    |> validate_format(:email, @email_regex, [message: "Email format isn't valid!"])
+    |> validate_length(:password, [min: 6, message: "Password should be at least 6 character(s)."])
+  end
+end
